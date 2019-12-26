fc65e66503f138c8aa2ff886aa770c4e8159e92b
Josh Mantovani <josh-mantovani@Joshs-MacBook-Pro.local>
2019-12-26 16:37:59 -0700

    placed starting files on github

---
+++ src/denver_data_science_url_loop.py
@@ -0,0 +1,18 @@
+import indeed_web_scrape_script as iwss
+import time
+from bs4 import BeautifulSoup
+
+starting_url = 'https://www.indeed.com/jobs?q=data+science&l=Denver%2C+CO&limit=50&radius=25'
+start = [num for num in range(50, 251, 50)]
+url_list = [starting_url]
+
+for i in start:
+    base_url = starting_url
+    page_num = f'&start={i}'
+    base_url += page_num
+    url_list.append(base_url)
+
+for url in url_list:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, features='html.parser')
+    job_temp, companies_temp, locations_temp = iwss.extract_title_company_location(soup)
