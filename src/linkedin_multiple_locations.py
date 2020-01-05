ceeef6873142427f30b50f2573cbb0883a4f4f05
Josh Mantovani <josh.mantova@gmail.com>
2020-01-04 18:48:19 -0700

    working version of linkedin script

--- src/linkedin_multiple_locations.py
+++ src/linkedin_multiple_locations.py
@@ -1,35 +1,51 @@
+from selenium import webdriver
 import selenium_linkedin_jobs_test as sljt
 import pandas as pd
+from selenium.common.exceptions import NoSuchElementException
+import time
 
 if __name__ == '__main__':
-    list_of_locations = ['Colorado', 'California', 'New%20york', 'Washington', 'Utah', 'Florida']
-    # for loc in list_of_locations:
-    #     print(f'starting to scrape {loc[1]} jobs from Linkedin')
-    #     search_result_url = f"https://www.linkedin.com/jobs/search?keywords=Data%20Science&location={loc}&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"
-    #     job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url)
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
 
-    #     df = pd.DataFrame()
-    #     df['Job_Title'] = job_titles
-    #     df['Company'] = company_names
-    #     df['Location'] = locations
-    #     df['Number_of_Applicants'] = num_applicants
-    #     df['Description'] = descriptions
+    list_of_locations = ['Colorado','California', 'New%20york', 'Washington', 'Utah', 'Florida']
+    for loc in list_of_locations:
+        print(f'Starting to scrape {loc} jobs from Linkedin')
+        search_result_url = f"https://www.linkedin.com/jobs/search?keywords=Data%20Science&location={loc}&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"
+        driver.get(search_result_url)
+        sljt.see_more_jobs(driver, num_times=2)
+        try:
+            job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url, driver)
+        except NoSuchElementException:
+            print('There was a No Such Element Exception error!! Waiting two minutes.')
+            time.sleep(120)
+            print('Continuing')
+            job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url, driver)
 
-    #     df.to_csv(f'df_linkedin_{loc}.csv')
-    #     print(f'{loc} scraping is done!')
-    # print('Finished scraping all locations!!!!')
+        df = pd.DataFrame()
+        df['Job_Title'] = job_titles
+        df['Company'] = company_names
+        df['Location'] = locations
+        df['Number_of_Applicants'] = num_applicants
+        df['Description'] = descriptions
 
-    loc = list_of_locations[1]
-    print(f'starting to scrape {loc} jobs from Linkedin')
-    search_result_url = f"https://www.linkedin.com/jobs/search?keywords=Data%20Science&location={loc}&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"
-    job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url)
+        df.to_csv(f'../Datasets/df_linkedin_{loc}_TEST.csv')
+        print(f'{loc} scraping is done!')
+        time.sleep(60)
+    print('Finished scraping all locations!!!!')
+    driver.close()
 
-    df = pd.DataFrame()
-    df['Job_Title'] = job_titles
-    df['Company'] = company_names
-    df['Location'] = locations
-    df['Number_of_Applicants'] = num_applicants
-    df['Description'] = descriptions
+    # loc = list_of_locations[1]
+    # print(f'starting to scrape {loc} jobs from Linkedin')
+    # search_result_url = f"https://www.linkedin.com/jobs/search?keywords=Data%20Science&location={loc}&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"
+    # job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url)
 
-    df.to_csv(f'df_linkedin_{loc}.csv')
-    print(f'{loc} scraping is done!')
+    # df = pd.DataFrame()
+    # df['Job_Title'] = job_titles
+    # df['Company'] = company_names
+    # df['Location'] = locations
+    # df['Number_of_Applicants'] = num_applicants
+    # df['Description'] = descriptions
+
+    # df.to_csv(f'df_linkedin_{loc}.csv')
+    # print(f'{loc} scraping is done!')
--- src/selenium_linkedin_jobs_test.py
+++ src/selenium_linkedin_jobs_test.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import time
 import pandas as pd
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+import numpy as np
 
 def see_more_jobs(driver, num_times=40):
     """
@@ -14,21 +16,20 @@ def see_more_jobs(driver, num_times=40):
     if num_times == 0:
         return None
     for _ in range(num_times):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(.5)
         see_more_jobs = driver.find_element_by_class_name('see-more-jobs')
         see_more_jobs.click()
-        time.sleep(2)
+        time.sleep(3)
 
-# see_more_jobs(20)
 
-def scrape_all_from_linkedin(search_result_url, driver, num_pages=40):
+def scrape_all_from_linkedin(search_result_url, driver):
     """
     Scrapes several features of jobs from linkedin search.
 
     :params: \n
     search_result_url: initial search page for job \n
-    num_pages: the number of times the user would like to press the 'see more jobs' button
+    driver: selenium webdriver to use to scrape
 
     :returns: \n
     job_titles: list of job titles of each job \n
@@ -37,7 +38,8 @@ def scrape_all_from_linkedin(search_result_url, driver, num_pages=40):
     num_applicants: list of number of applicants for each job \n
     descriptions: list of descriptions of each job
     """
-
+    delays = [2, 4, 6, 2, 2, 19, 5]
+    delay = np.random.choice(delays)
     jobs = driver.find_elements_by_class_name('result-card__full-card-link')
 
     job_titles = []
@@ -47,38 +49,48 @@ def scrape_all_from_linkedin(search_result_url, driver, num_pages=40):
     descriptions = []
 
     for job in jobs:
+        driver.execute_script('return arguments[0].scrollIntoView();', job)
+        time.sleep(delay)
         job.click()
-        time.sleep(2)
-        job_title_h2 = driver.find_element_by_class_name('topcard__title')
-        company_name_a = driver.find_element_by_class_name('topcard__org-name-link')
-        locations_span = driver.find_element_by_xpath("//span[contains(@class, 'topcard__flavor--bullet')]")
+        # try:
+        #     job.click()
+        # except StaleElementReferenceException:
+        #     print('Stale element reference exception!!')
+        #     time.sleep(20)
+        #     job.click()
+        #     print('Continuing')
+        job_title_h2 = driver.find_element_by_class_name('topcard__title').text
+        company_name_a = driver.find_element_by_class_name('topcard__org-name-link').text
+        time.sleep(delay)
+        locations_span = driver.find_element_by_xpath("//span[contains(@class, 'topcard__flavor--bullet')]").text
         try: 
             # if more than 25 people applied, the number of applicants is located here
-            num_applicants_span = driver.find_element_by_xpath("//span[contains(@class, 'topcard__flavor--bullet') and contains(@class, 'num-applicants__caption')]")
-        except NoSuchElementException as exception:
+            num_applicants_span = driver.find_element_by_xpath("//span[contains(@class, 'topcard__flavor--bullet') and contains(@class, 'num-applicants__caption')]").text
+        except NoSuchElementException:
             # if less than 25 people applied, the number of applicants is located here
-            num_applicants_span = driver.find_element_by_class_name('num-applicants__caption')
-        description = driver.find_element_by_class_name('description__text')
-        job_titles.append(job_title_h2.text)
-        company_names.append(company_name_a.text)
-        locations.append(locations_span.text)
-        num_applicants.append(num_applicants_span.text)
-        descriptions.append(description.text)
+            num_applicants_span = driver.find_element_by_class_name('num-applicants__caption').text
+        description = driver.find_element_by_class_name('description__text').text
+        time.sleep(delay)
+        job_titles.append(job_title_h2)
+        company_names.append(company_name_a)
+        locations.append(locations_span)
+        num_applicants.append(num_applicants_span)
+        descriptions.append(description)
 
     return job_titles, company_names, locations, num_applicants, descriptions
 
 if __name__ == '__main__':
-    search_result_url = 'https://www.linkedin.com/jobs/search?keywords=Data%20Science&location=Colorado&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0'
-    job_titles, company_names, locations, num_applicants, descriptions = scrape_all_from_linkedin(search_result_url, num_pages=0)
+    # search_result_url = 'https://www.linkedin.com/jobs/search?keywords=Data%20Science&location=Colorado&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0'
+    # job_titles, company_names, locations, num_applicants, descriptions = scrape_all_from_linkedin(search_result_url, num_pages=0)
 
-    df = pd.DataFrame()
-    df['Job_Title'] = job_titles
-    df['Company'] = company_names
-    df['Location'] = locations
-    df['Number_of_Applicants'] = num_applicants
-    df['Description'] = descriptions
+    # df = pd.DataFrame()
+    # df['Job_Title'] = job_titles
+    # df['Company'] = company_names
+    # df['Location'] = locations
+    # df['Number_of_Applicants'] = num_applicants
+    # df['Description'] = descriptions
 
-    print(df)
+    # print(df)
     # df.to_csv('test_linkedin_scrape.csv')
 
     """
