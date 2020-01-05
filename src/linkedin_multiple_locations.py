37704cb7a8a3723a6a092e5e15f1a089c7cab558
Josh Mantovani <josh.mantova@gmail.com>
2020-01-05 16:01:23 -0700

    linkedin scraping complete dfs added

--- src/linkedin_multiple_locations.py
+++ src/linkedin_multiple_locations.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import selenium_linkedin_jobs_test as sljt
+import Selenium_and_BS4_linkedin as sbs4
 import pandas as pd
 from selenium.common.exceptions import NoSuchElementException
 import time
@@ -8,7 +9,7 @@ import numpy as np
 if __name__ == '__main__':
     driver = webdriver.Chrome()
     driver.implicitly_wait(5)
-    delays = np.arange(2,15)
+    delays = np.arange(2,5)
     delay = np.random.choice(delays)
 
     list_of_locations = ['Colorado','California', 'New%20york', 'Washington', 'Utah', 'Florida']
@@ -17,13 +18,19 @@ if __name__ == '__main__':
         search_result_url = f"https://www.linkedin.com/jobs/search?keywords=Data%20Science&location={loc}&trk=guest_job_search_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"
         driver.get(search_result_url)
         sljt.see_more_jobs(driver, num_times=20)
-        try:
-            job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url, driver)
-        except NoSuchElementException:
-            print('There was a No Such Element Exception error!! Waiting two minutes.')
-            time.sleep(120)
-            print('Continuing')
-            job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url, driver)
+
+        time.sleep(delay)
+
+        job_titles, company_names, locations, num_applicants, descriptions = sbs4.scrape_all_from_linkedin(driver)
+
+
+        # try:
+        #     job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url, driver)
+        # except NoSuchElementException:
+        #     print('There was a No Such Element Exception error!! Waiting two minutes.')
+        #     time.sleep(120)
+        #     print('Continuing')
+        #     job_titles, company_names, locations, num_applicants, descriptions = sljt.scrape_all_from_linkedin(search_result_url, driver)
 
         df = pd.DataFrame()
         df['Job_Title'] = job_titles
